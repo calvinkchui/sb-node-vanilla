ce398403095772194e35613b3ef0d9f6cedddd84
nivlachui <calvin.hui2@gmail.com>
2022-09-21 15:44:17 +0800

    update

--- common/demoContainer.js
+++ common/demoContainer.js
@@ -3,7 +3,7 @@ import Mustache from 'mustache';
 // The requested module '/common/demoContainer.js?t=1663571718180' does not provide an export named 'default'
 // Fix: add "default"
 
-export default function showDemo(demoContent) {
+export default function showDemoContent(demoContent) {
   var demo = demoContent;
   console.log('showDemo', demo);
 
