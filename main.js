cf9e4ca168181f66da47db5320603157a08a012e
calvinkchui <96161394+calvinkchui@users.noreply.github.com>
2022-10-11 16:01:26 +0800

    1 file modified, 1 added

--- main.js
+++ main.js
@@ -20,6 +20,7 @@ import { jsonataDemoConfig } from './pkg/jsonata.js';
 import { mustacheDemoConfig } from './pkg/mustache.js';
 import { axiosDemoConfig } from './pkg/axios.js';
 import { sanityClientDemoConfig } from './pkg/sanityClient.js';
+//import { luxonDemoConfig } from './pkg/luxon.js';
 
 var html = '<button id="mustcache" type="button">Mustacahe</button>';
 
@@ -38,76 +39,88 @@ document.querySelector('#vite').addEventListener('click', () => {
 //demoMustache(document.querySelector('#mustcache'));
 
 //console.log(demoMustache);
-document.querySelector('#mustcache')
-   .addEventListener('click', () => renderDemoContent(mustacheDemoConfig));
+document
+  .querySelector('#mustcache')
+  .addEventListener('click', () => renderDemoContent(mustacheDemoConfig));
 
 var mainConfig = {
-  category: [ {
-    title: "sandox",
-    package: [
-      {
-        title: 'Vite - Vanilla',
-        description: "JS",        
-        id: 'jsVanilla',
-        config: jsVanillaDemoConfig,
-      },
-      {
-        title: 'Fetch',
-        description: "fetch",        
-        id: 'jsFetch',
-        config: jsFetchDemoConfig,
-      },
-    ],
+  category: [
+    {
+      title: 'sandox',
+      package: [
+        {
+          title: 'Vite - Vanilla',
+          description: 'JS',
+          id: 'jsVanilla',
+          config: jsVanillaDemoConfig,
+        },
+        {
+          title: 'Fetch',
+          description: 'fetch',
+          id: 'jsFetch',
+          config: jsFetchDemoConfig,
+        },
+      ],
     },
     {
-    title: "Js",
-    package: [
+      title: 'Js',
+      package: [
+        {
+          title: 'Axios',
+          description: 'Promise based HTTP client for the browser and node.js',
+          logo: 'https://axios-http.com/assets/logo.svg',
+          link: [{ title: 'www', url: 'https://github.com/axios/axios' }],
+          id: 'js_axios',
+          config: axiosDemoConfig,
+        },
+        {
+          title: 'Mustache',
+          description: 'Logic-less {{mustache}} templates with JavaScript',
+          logo: 'https://i0.wp.com/css-tricks.com/wp-content/uploads/2016/01/mustache-logo.png',
+          link: [{ title: 'www', url: 'https://github.com/janl/mustache.js/' }],
+          id: 'js_mustache',
+          config: mustacheDemoConfig,
+        },
+        {
+          title: '@sanity/client',
+          description: 'Javascript client for Sanity.',
+          logo: 'http://www.sanity.io/static/images/logo.png',
+          link: [
+            {
+              title: 'www',
+              url: 'https://www.npmjs.com/package/@sanity/client',
+            },
+          ],
+          id: 'js_sanity',
+          config: sanityClientDemoConfig,
+        },
+        {
+          title: 'JSONata',
+          description:
+            'JSONata is a lightweight query and transformation language for JSON data.',
+          logo: 'http://docs.jsonata.org/img/jsonata-button.png',
+          link: [
+            { title: 'www', url: 'http://docs.jsonata.org/using-nodejs' },
+            { title: 'try', url: 'https://try.jsonata.org/' },
+          ],
+          id: 'js_jsonata',
+          config: jsonataDemoConfig,
+        },
+        /*               
       {
-        title: 'Axios',
-        description: "Promise based HTTP client for the browser and node.js",
-        logo: "https://axios-http.com/assets/logo.svg",
+        title: 'Luxon',
+        description: "A powerful, modern, and friendly wrapper for JavaScript dates and times.",        
+        logo: "https://moment.github.io/luxon/docs/_media/Luxon_icon_64x64.png",
         link: [
-          { title: "www", url: "https://github.com/axios/axios" },         
+          { title: "www", url: "https://moment.github.io/luxon/#/" },                   
         ],     
-        id: 'js_axios',
-        config: axiosDemoConfig,
-      },
-      {
-        title: 'Mustache',
-        description: "Logic-less {{mustache}} templates with JavaScript",
-        logo: "https://i0.wp.com/css-tricks.com/wp-content/uploads/2016/01/mustache-logo.png",
-        link: [
-          { title: "www", url: "https://github.com/janl/mustache.js/" },         
-        ],     
-        id: 'js_mustache',
-        config: mustacheDemoConfig,
-      },
-      {
-        title: '@sanity/client',
-        description: "Javascript client for Sanity.",        
-        logo: "http://www.sanity.io/static/images/logo.png",
-        link: [
-          { title: "www", url: "https://www.npmjs.com/package/@sanity/client" },         
-        ],     
-        id: 'js_sanity',
-        config: sanityClientDemoConfig,
-      },
-      {
-        title: 'JSONata',
-        description: "JSONata is a lightweight query and transformation language for JSON data.",        
-        logo: "http://docs.jsonata.org/img/jsonata-button.png",
-        link: [
-          { title: "www", url: "http://docs.jsonata.org/using-nodejs" },         
-          { title: "try", url: "https://try.jsonata.org/" },                   
-        ],     
-        id: 'js_jsonata',
-        config: jsonataDemoConfig,
-      },            
-
-    ],
-  }]
-};  
+        id: 'js_luxon',
+        config: luxonDemoConfig,
+      },   
+*/
+      ],
+    },
+  ],
+};
 
 renderDemoMain(mainConfig);
-
-
---
+++ pkg/luxon.js
@@ -0,0 +1,20 @@
+import luxon from 'luxon';
+
+let luxonDemoConfig = {
+  title: 'Luxon',
+  category: [
+    {
+      sample: [
+        {
+          title: 'Basic Sample code',
+          sampleCode: 'Datetime',
+          id: 'luxon_0001',
+          demoScript: function () {},
+          output: 'console',
+        },
+      ],
+    },
+  ],
+};
+
+export { luxonDemoConfig };
